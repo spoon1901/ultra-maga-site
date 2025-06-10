a34dfe67650f7af262d2a33ebee112dde2bd711d
spoon1901 <james.witherspoon1@gmail.com>
2025-06-09 22:38:15 -0600

    Update Tokenomics.js

--- ultra-maga-site/src/components/Tokenomics.js
+++ ultra-maga-site/src/components/Tokenomics.js
@@ -5,8 +5,10 @@ export default function Tokenomics() {
   return (
     <section id="tokenomics" className="relative w-full py-20 px-6 text-white text-center">
       <div className="bg-black bg-opacity-60 backdrop-blur-sm border border-red-600 rounded-xl p-8 shadow-lg max-w-4xl mx-auto">
-        <h2 className="text-4xl font-bold mb-6 drop-shadow">TOKENOMICS</h2>
-        <div className="text-lg space-y-4 text-left">
+        <h2 className="text-4xl sm:text-4xl md:text-5xl lg:text-4xl font-bold mb-6 text-white drop-shadow">
+TOKENOMICS</h2>
+        <div className="text-xl sm:text-3xl md:text-4xl lg:text-2xl text-white space-y-4 text-left">
+
           <p><strong>Name:</strong> ULTRA $MAGA</p>
           <p><strong>Issuer:</strong> rwH49FHnr48FeUP7NX9EuL4k1peLrPwS3d</p>
           <p><strong>Total Supply:</strong> 99.9M $MAGA</p>
