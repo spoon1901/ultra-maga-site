cc91d1760851385316daf3a26f2d7cfce5bfe093
spoon1901 <james.witherspoon1@gmail.com>
2025-06-14 11:31:03 -0600

    Update game.js

--- ultra-maga-site/public/maga-game/game.js
+++ ultra-maga-site/public/maga-game/game.js
@@ -17,74 +17,70 @@ const config = {
   }
 };
 
-let trump, cursors, pipes, score = 0, scoreText, gameOver = false;
-let bg;
-let startText, restartText, music, gameStarted = false;
+let trump, cursors, pipes, scoreText, gameOver = false;
+let score = 0;
+let startText, restartText, background;
+let music;
 
 const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('trump', 'https://files.catbox.moe/kbcy6t.png');
   this.load.image('pipe', 'https://files.catbox.moe/qswcqq.png');
-  this.load.image('bg', 'https://files.catbox.moe/chw14r.png');
-  this.load.audio('bgm', 'https://files.catbox.moe/4eq3qy.mp3');
+  this.load.image('background', 'https://files.catbox.moe/chw14r.png');
+  this.load.audio('music', 'https://files.catbox.moe/4eq3qy.mp3');
 }
 
 function create() {
-  const screenWidth = this.scale.width;
-  const screenHeight = this.scale.height;
+  // Background
+  background = this.add.tileSprite(0, 0, config.width, config.height, 'background').setOrigin(0);
 
-  bg = this.add.tileSprite(0, 0, screenWidth, screenHeight, 'bg').setOrigin(0).setScrollFactor(0);
+  // Music
+  music = this.sound.add('music', { loop: true, volume: 0.2 });
+  music.play();
 
-  trump = this.physics.add.sprite(100, screenHeight / 2, 'trump').setScale(0.07);
-  trump.body.setSize(trump.width * 0.6, trump.height * 0.6, true);
+  // Trump sprite
+  trump = this.physics.add.sprite(100, 300, 'trump').setScale(0.07);
+  trump.body.setSize(trump.width * 0.7, trump.height * 0.7);
   trump.setCollideWorldBounds(true);
   trump.setVisible(false);
+  trump.body.allowGravity = false;
 
+  // Pipes group
   pipes = this.physics.add.group();
 
-  scoreText = this.add.text(screenWidth / 2, screenHeight * 0.05, 'Score: 0', {
-    fontSize: '24px',
+  // Score
+  scoreText = this.add.text(config.width / 2, 20, '0', {
+    fontSize: '32px',
     fill: '#fff'
-  }).setOrigin(0.5).setDepth(10).setVisible(false);
+  }).setOrigin(0.5).setDepth(1);
 
-  startText = this.add.text(screenWidth / 2, screenHeight / 2, 'TAP TO START', {
+  // Start text
+  startText = this.add.text(config.width / 2, config.height / 2, 'TAP TO START', {
     fontSize: '28px',
-    fill: '#ffffff'
-  }).setOrigin(0.5).setDepth(10);
+    fill: '#ffff00'
+  }).setOrigin(0.5).setDepth(1);
 
-  restartText = this.add.text(screenWidth / 2, screenHeight / 2 + 40, 'CLICK TO TRY AGAIN', {
-    fontSize: '20px',
-    fill: '#ff0000'
-  }).setOrigin(0.5).setDepth(10).setVisible(false);
-
-  cursors = this.input.keyboard.createCursorKeys();
-
+  // Input
   this.input.on('pointerdown', () => {
-    if (!gameStarted) {
+    if (!trump.visible) {
       startGame.call(this);
     } else if (gameOver) {
-      this.scene.restart();
+      restartGame.call(this);
     } else {
       flap();
     }
   });
 
+  // Keyboard
+  cursors = this.input.keyboard.createCursorKeys();
   this.physics.add.collider(trump, pipes, hitPipe, null, this);
-
-  music = this.sound.add('bgm', { loop: true, volume: 0.3 });
-  music.play();
 }
 
 function startGame() {
-  gameStarted = true;
-  trump.setVisible(true);
-  scoreText.setVisible(true);
   startText.setVisible(false);
-  restartText.setVisible(false);
-  gameOver = false;
-  score = 0;
-  scoreText.setText('Score: 0');
+  trump.setVisible(true);
+  trump.body.allowGravity = true;
 
   this.time.addEvent({
     delay: 1500,
@@ -94,22 +90,8 @@ function startGame() {
   });
 }
 
-function update() {
-  if (!gameOver && trump.visible) {
-    if (cursors.space && Phaser.Input.Keyboard.JustDown(cursors.space)) {
-      flap();
-    }
-
-    pipes.getChildren().forEach(pipe => {
-      if (pipe.x < trump.x && !pipe.passed) {
-        score += 1;
-        pipe.passed = true;
-        scoreText.setText('Score: ' + score);
-      }
-    });
-
-    bg.tilePositionX += 1;
-  }
+function restartGame() {
+  this.scene.restart();
 }
 
 function flap() {
@@ -117,13 +99,11 @@ function flap() {
 }
 
 function addPipe() {
-  const minY = 100;
-  const maxY = config.height - 100;
-  const y = Phaser.Math.Between(minY, maxY);
   const gap = config.height / 4;
+  const y = Phaser.Math.Between(150, config.height - 150);
 
-  const topPipe = pipes.create(config.width, y - gap, 'pipe');
-  const bottomPipe = pipes.create(config.width, y + gap, 'pipe');
+  const topPipe = pipes.create(config.width, y - gap, 'pipe').setOrigin(0, 1);
+  const bottomPipe = pipes.create(config.width, y + gap, 'pipe').setOrigin(0, 0);
 
   [topPipe, bottomPipe].forEach(pipe => {
     pipe.setScale(0.15);
@@ -131,13 +111,38 @@ function addPipe() {
     pipe.passed = false;
     pipe.body.allowGravity = false;
     pipe.setImmovable(true);
-    pipe.body.setSize(pipe.width * 0.8, pipe.height * 1.0, true);
+    pipe.body.setSize(pipe.width * 0.15, pipe.height * 0.6);
+  });
+}
+
+function update() {
+  if (gameOver) return;
+
+  background.tilePositionX += 1;
+
+  if (cursors.space && Phaser.Input.Keyboard.JustDown(cursors.space)) {
+    flap();
+  }
+
+  pipes.getChildren().forEach(pipe => {
+    if (!pipe.passed && pipe.x + pipe.width < trump.x) {
+      pipe.passed = true;
+      score += 1;
+      scoreText.setText(score);
+    }
   });
 }
 
 function hitPipe() {
   if (gameOver) return;
+
   gameOver = true;
   this.physics.pause();
-  restartText.setVisible(true);
+
+  trump.setTint(0xff0000);
+
+  restartText = this.add.text(config.width / 2, config.height / 2 + 50, 'CLICK TO TRY AGAIN', {
+    fontSize: '20px',
+    fill: '#ff0000'
+  }).setOrigin(0.5).setDepth(1);
 }
