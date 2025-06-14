8c1d4bd9dd31750a27f6b5bb057e53b12474a378
spoon1901 <james.witherspoon1@gmail.com>
2025-06-14 13:07:35 -0600

    Update game.js

--- ultra-maga-site/public/maga-game/game.js
+++ ultra-maga-site/public/maga-game/game.js
@@ -118,8 +118,8 @@ function addPipe() {
     pipe.passed = false;
     pipe.body.allowGravity = false;
     pipe.setImmovable(true);
-    pipe.body.setSize(pipe.displayWidth * 1.3, pipe.displayHeight * 2);
-    pipe.body.setOffset(-pipe.displayWidth * 0.15, -pipe.displayHeight);
+    pipe.body.setSize(pipe.displayWidth * 1.3, pipe.displayHeight * 2.0);
+    pipe.body.setOffset(-pipe.displayWidth * 1.3, -pipe.displayHeight);
     pipe.setDepth(1);
   });
 }
