5078302ac173605d3079d500472e4715a7e0aab3
spoon1901 <james.witherspoon1@gmail.com>
2025-06-27 10:16:11 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.css';  // ✅ This line is the magic
-
+import './style.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
